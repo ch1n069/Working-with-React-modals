f85ae085b4fdabc5bb520f4abf03ac429a6aa80f
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-19 15:18:22 +0300

    wrapped the userslist into a card

--- src/components/userslist/Userslist.js
+++ src/components/userslist/Userslist.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Card from "../UI/Card";
 
 const Userslist = (props) => {
-  return <div>Userslist</div>;
+  return (
+    <Card>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.age})
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
 };
 
 export default Userslist;
