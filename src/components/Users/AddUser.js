b6a6984b678fbc7335a6cfb28547a6074ce3c7b3
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-19 14:11:23 +0300

    created adduser component

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import classes "./AddUser.module.css"
+
+// all imports happen above
+const AddUser = () => {
+  return (
+    <form action="" onSubmit="addUserHandler">
+      <label htmlFor="username">Enter your Name</label>
+      <input id="username" type="text" name="" />
+      <label htmlFor="age">Enter your age</label>
+      <input id="age" type="number" />
+      <button type="submit">Add user</button>
+    </form>
+  );
+};
+
+export default AddUser;
