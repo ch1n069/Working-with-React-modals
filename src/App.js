3873d7775c4e946324e93e71117c1a87b317c9d2
Bruno Alpachino <brunolalpachino@gmail.com>
2022-10-19 15:53:45 +0300

    created function to help pass state up to the parent component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import AddUser from "./components/Users/AddUser";
 import Userslist from "./components/Users/Userslist";
-
+import { useState } from "react-intl";
+const users = [];
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge }];
+    });
+  };
+
+  // functions go above
   return (
     <div>
-      <AddUser />
-      <Userslist />
+      <AddUser onAddUser={addUserHandler} />
+      <Userslist users={usersList} />
     </div>
   );
 }
